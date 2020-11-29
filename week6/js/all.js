0e60c693285ec4a03654750d280a14cf2b244b55
KevinChang <apog780212@gmail.com>
2020-11-29 15:12:11 +0800

    <每週作業>

--- week6/js/all.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-
-    $('.showmenu').click(function (event) {
-        /*取消預設行為，也就是取消點擊連結後連去該網頁的行為*/
-        event.preventDefault();
-        $('body').toggleClass('menu-show');
-    });
-})
